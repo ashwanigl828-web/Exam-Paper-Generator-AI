27e3961e7f5b72ccc0ebaf468822398c7a6c1ff0
ashwanigl828-web <ashwanigl828@gmail.com>
2026-06-19 21:28:57 +0530

    Fix LangChain import path for text splitters

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ from googleapiclient.http import MediaIoBaseDownload
 
 # LangChain & FAISS
 from langchain_community.document_loaders import PyPDFLoader
-from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_google_genai import GoogleGenerativeAIEmbeddings
 from langchain_community.vectorstores import FAISS
 
